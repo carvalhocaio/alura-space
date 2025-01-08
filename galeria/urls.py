203687d4f1538b4a87861d9f8eadcfb061cc257e
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-01-08 11:38:12 -0400

    Update galeria app: add imagem view, update index view, and modify URLs; enhance static file handling and add new styles

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
-urlpatterns = [path('', index)]
+urlpatterns = [
+    path('', index),
+    path('imagem/', imagem)
+]
